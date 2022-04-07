c6f81e49c17dc95e45926895163bf52c71f7eaa9
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-04-07 11:25:05 +0900

    Initial commit

---
+++ src/main/java/com/roy/optimizing/OptimizingApplication.java
@@ -0,0 +1,13 @@
+package com.roy.optimizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roy/optimizing/OptimizingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.optimizing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
